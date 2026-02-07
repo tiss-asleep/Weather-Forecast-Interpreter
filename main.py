e6ab8a5de9d43a2bc3c785f46c41f8d006f96d94
tiss-asleep <jambalm0674@gmail.com>
2026-02-06 20:26:35 -0500

    Added constants for the url of current weather and forecast weather, also refactored some code

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import requests
 
 API_KEY = "7eddd10bc7244779ae104325260702"
+CURRENT = "https://api.weatherapi.com/v1/current.json"
+FORECAST = "https://api.weatherapi.com/v1/forecast.json"
 
 def get_response(city_name):
-    url = "https://api.weatherapi.com/v1/current.json"
     params = {
         "key": API_KEY,
         "q": city_name
     }
 
-    return requests.get(url, params=params)
+    return requests.get(CURRENT, params=params)
 
 def get_current(response):
     current = response.json()["current"]
 
-    print(f"Temperature: {current["temp_c"]}C")
-    print(f"Humidity: {current["humidity"]}%")
-    print(f"Wind: {current["wind_kph"]}km/h")
+    print(f"Temperature: {current['temp_c']}C")
+    print(f"Humidity: {current['humidity']}%")
+    print(f"Wind: {current['wind_kph']} km/h")
 
 def main():
-    # city_name = input("Enter name of city to forecast >> ")
+    city_name = input("Enter city name >> ")
 
-    response = get_response("Rochester")
+    response = get_response(city_name)
 
-    if (response.status_code != 200):
+    if response.status_code != 200:
         print("Error fetching weather data")
         exit()
-
-    print("\n", response.text, "\n")
 
     get_current(response)
 
