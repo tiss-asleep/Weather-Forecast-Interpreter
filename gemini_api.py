de8cccbba4b5175c5340338c63db2a2a43f9611c
tiss-asleep <jambalm0674@gmail.com>
2026-02-06 23:17:15 -0500

    gemini_api:get_gemini_response commited

--- gemini_api.py
+++ gemini_api.py
@@ -6,4 +6,16 @@ API_KEY = os.getenv("GEMINI_API_KEY")
 if not API_KEY:
     raise RuntimeError("GEMINI_API_KEY is not set")
 
-client = genai.Client(api_key=API_KEY)
+CLIENT = genai.Client(api_key=API_KEY)
+
+def get_gemini_response(weather_data):
+    prompt = f"""
+    Raw weather data is provided below:
+    {weather_data}
+    Using it, write a short, user-friendly summary of it.
+    """
+
+    return CLIENT.models.generate_content(
+        model="gemini-1.5-flash",
+        contents=prompt
+    ).text
