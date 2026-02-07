803c987cb61a9d320c1348e1fc106fda8413a329
tiss-asleep <jambalm0674@gmail.com>
2026-02-06 20:41:05 -0500

    Removed main:get_request and refactored the rest of the code since both main:get_current and main:get_forecast will be using requests with different parameters

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ API_KEY = "7eddd10bc7244779ae104325260702"
 CURRENT_URL = "https://api.weatherapi.com/v1/current.json"
 FORECAST_URL = "https://api.weatherapi.com/v1/forecast.json"
 
-def get_response(city_name):
+def get_current(city_name):
     params = {
         "key": API_KEY,
         "q": city_name
     }
 
-    return requests.get(CURRENT_URL, params=params)
-
-def get_current(response):
+    response = requests.get(CURRENT_URL, params=params)
+    if response.status_code != 200:
+        print("Error fetching current weather data")
+        return
+    
     current = response.json()["current"]
 
-    print(f"Temperature: {current['temp_c']}C")
-    print(f"Humidity: {current['humidity']}%")
-    print(f"Wind: {current['wind_kph']} km/h")
+    print(f"\nCurrent temperature: {current['temp_c']}C")
+    print(f"Current humidity: {current['humidity']}%")
+    print(f"Current wind speeds: {current['wind_kph']} km/h")
 
 def get_forecast(city_name, days=3):
     params = {
@@ -34,26 +36,17 @@ def get_forecast(city_name, days=3):
     forecast_days = response.json()["forecast"]["forecastday"]
 
     for day in forecast_days:
-        date = day["date"]
-        max_temp = day["day"]["maxtemp_c"]
-        min_temp = day["day"]["mintemp_c"]
-        condition = day["day"]["condition"]["text"]
-        rain_chance = day["day"]["daily_chance_of_rain"]
-
-        print(f"\nDate: {date}")
-        print(f"Min: {min_temp}C, Max: {max_temp}C")
-        print(f"Condition: {condition}, Chance of rain: {rain_chance}%")
+        print(f"\nForecasted date: {day["date"]}")
+        print(f"Minimun temperature: {day["day"]["mintemp_c"]}C")
+        print(f"Maximum temperature: {day["day"]["maxtemp_c"]}C")
+        print(f"Condition: {day["day"]["condition"]["text"]}, Chance of rain: {day["day"]["daily_chance_of_rain"]}%")
 
 def main():
     city_name = input("Enter city name >> ")
 
-    response = get_response(city_name)
+    get_current(city_name)
 
-    if response.status_code != 200:
-        print("Error fetching weather data")
-        exit()
-
-    get_current(response)
+    get_forecast(city_name)
 
 if __name__ == "__main__":
     main()
